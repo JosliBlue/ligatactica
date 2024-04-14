51b8d8f181b4a26f13d8608405ca9754876b53f3
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2024-04-14 13:16:30 -0500

    Frontend login incorporado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,30 +3,67 @@
 @section('tittle', 'Login')
 
 @section('imports')
+    @vite('resources/css/complements/all.css')
+
     <!-- Import script and css of SWEETALERT component -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @vite('resources/css/components/sweetAlert.css')
+
+    @vite('resources/css/login.css')
+    @vite('resources/js/login.js')
 @endsection
 
 
 @section('content')
 
-    <form action="{{ route('postLogin') }}" method="post">
-        @csrf
-        <label for="email">Nombre o correo electronico</label>
-        <input type="email" name="email" id="email">
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form>
+                <h1>Sobre nosotros</h1>
+                <!--
+                    <div class="social-icons">
+                        <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                        <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                        <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+                -->
 
-        <label for="password">Contraseña</label>
-        <input type="password" name="password" id="password">
-        @if ($errors->has('errorCredentials'))
-            @component('_components.alert')
-                @slot('icon', 'error')
-                @slot('message')
-                    {{ $errors->first('errorCredentials') }}
-                @endslot
-            @endcomponent
-        @endif
-        <input type="submit" value="Login">
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form action="{{ route('postLogin') }}" method="post">
+                @csrf
+                <h1>Iniciar sesion</h1>
 
-    </form>
+                <label for="email">Correo electronico</label>
+                <input type="email" name="email" id="email" placeholder="Ingresa tu correo electronico">
+                <label for="password">Contraseña</label>
+                <input type="password" name="password" id="password" placeholder="Ingresa tu contraseña">
+                @if ($errors->has('errorCredentials'))
+                    @component('_components.alert')
+                        @slot('icon', 'error')
+                        @slot('message')
+                            {{ $errors->first('errorCredentials') }}
+                        @endslot
+                    @endcomponent
+                @endif
+                <input type="submit" value="Iniciar sesion">
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <div class="imgLogo"></div>
+                    <button class="hidden" id="login">Regresar</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>{{ env('NOMBRE_LIGA') }}</h1>
+                    <p>Bienvenido, accede para gestionar tu equipo</p>
+                    <button class="hidden" id="register">Saber màs</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
